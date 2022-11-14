7f2a6e16f457cc04c9222559435af28879ac40a4
Васев Дмитрий Андреевич <DAVasev@sberbank.ru>
2022-11-14 23:00:45 +0300

    Add docker and compose configurations

--- gateway-service/build.gradle.kts
+++ gateway-service/build.gradle.kts
@@ -25,7 +25,6 @@ dependencies {
     implementation(project(":rental-api"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
